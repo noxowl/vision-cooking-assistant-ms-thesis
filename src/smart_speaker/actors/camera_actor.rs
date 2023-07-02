84371b8c590cd2ebfb7dfc415a708f01214cce7f
Suyeong RHIE <10545351+noxowl@users.noreply.github.com>
2023-07-03 02:33:40 +0900

    remove old code comment

--- src/smart_speaker/actors/camera_actor.rs
+++ src/smart_speaker/actors/camera_actor.rs
@@ -65,32 +65,14 @@ impl CameraActor {
                                                         send_to: _,
                                                         frame_data_bytes: _,
                                                         height: _, }) => {
-                camera_frame_message(&self.sender, SmartSpeakerActors::CameraActor, send_from,
-                                     self.frame_data_bytes.clone(), self.height.clone());
+                camera_frame_message(
+                    &self.sender,
+                    SmartSpeakerActors::CameraActor,
+                    send_from,
+                    self.frame_data_bytes.clone(),
+                    self.height.clone());
             },
             _ => {}
         }
     }
 }
-
-//  fn capture_frame(capture: &mut visio::VisioCapture, frame: Arc<Mutex<Mat>>, gaze: Arc<Mutex<Point2f>>, halt: &Cell<bool>) {
-//     while !halt.get() {
-//         match capture.capture() {
-//             Ok(m) => {
-//                 {
-//                     let mut frame_lock = frame.lock().unwrap();
-//                     *frame_lock = m;
-//                 }
-//                 match capture.capture_gaze() {
-//                     Ok(g) => {
-//                         let mut gaze_lock = gaze.lock().unwrap();
-//                         *gaze_lock = g;
-//                     }
-//                     _ => {}
-//                 }
-//             },
-//             _ => {}
-//         }
-//         tokio::time::sleep(Duration::from_millis(33));
-//     }
-// }
